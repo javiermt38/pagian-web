6fd110ea1a0a787163a9f76eb8b659e6cd08a1c9
Javier <andresrincon_34@hotmail.com>
2024-07-23 15:33:06 -0500

    Subiendo Proyecto Pagina Web

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+const datos = {
+    nombre: '',
+    celular: '',
+    correo: '',
+    mensaje: ''
+}
+
+// Eventos de los input
+const nombre = document.querySelector('#nombre');
+const celular = document.querySelector('#celular');
+const correo = document.querySelector('#correo');
+const mensaje = document.querySelector('#mensaje');
+// console.log(mensaje);
+
+// prueba eventListener
+// nombre.addEventListener('input', prueba);
+// function prueba(){
+//     alert('ingresaste el nombre');
+// }
+
+nombre.addEventListener('input', leerTexto);
+celular.addEventListener('input', leerTexto);
+correo.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(evento){
+    // console.log(evento);
+    datos[evento.target.id] = evento.target.value;
+    // console.log(datos);
+}
+
+const formulario = document.querySelector('.formulario');
+// console.log(formulario);
+
+formulario.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    //--- destructuring
+    const { nombre, celular, correo, mensaje } = datos;
+    //console.log(nombre, celular, correo, mensaje);
+    if (nombre === '' || celular === '' || correo === '' || mensaje === '') {
+        // console.log('todos los campos son obligatorios');
+        mostrarError('Diligencia todos los campos');
+        return; //--- corta la ejecucion del codigo
+    }
+    mostrarMensaje("Datos enviados correctamente");
+    function mostrarMensaje(mensaje){
+        let alerta = document.createElement("div");
+        alerta.textContent = mensaje
+        alerta.classList.add("correcto")
+        formulario.appendChild(alerta);
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000)
+    }
+
+    function mostrarError(mensaje){
+        //console.log(mensaje);
+        let error = document.createElement('div'); 
+        error.textContent = mensaje; 
+        error.classList.add('error');
+        // console.log(error);
+        formulario.appendChild(error);
+        setTimeout(() => {
+            error.remove();
+        }, 5000);
+    }
+
+
+})
+
+
